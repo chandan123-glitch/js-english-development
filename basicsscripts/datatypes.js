8933016a2bac666a917d1fe9ecc5265a7baceb6a
chandan123-glitch <cnaik1646@gmail.com>
2025-03-20 06:45:16 +0000

    data

---
+++ basicsscripts/datatypes.js
@@ -0,0 +1,13 @@
+let name = "Chandan"
+let age = 54
+let isLoggedIn = true
+
+
+console.log(typeof "Chandan");
+console.log(typeof 8768878567);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
