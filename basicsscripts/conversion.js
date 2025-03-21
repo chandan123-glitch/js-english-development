a954d2d90ac8ea07dd5f3dc22163c84cb5abde4e
chandan123-glitch <cnaik1646@gmail.com>
2025-03-21 13:16:18 +0000

    datatypes summary

---
+++ basicsscripts/conversion.js
@@ -0,0 +1,31 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//To convert a number -> can be easily converted (33 -> 33)
+//To convert a "not a number" -> NaN ("33abc" -> NaN)
+//To convert a "true" value -> 1
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+
+let newNumber = 43
+
+let someString = String(newNumber)
+
+console.log(someString);
+
+console.log(typeof someString)
+
+
+
+
+
